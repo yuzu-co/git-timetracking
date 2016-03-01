5b6434a09a5337f45ae9eac4b21571b7e09f1c79
Jonathan Martin <jmartin@iw2.fr>
2016-03-01 13:38:32 +0100

    Update README and install script

---
+++ README.md
@@ -0,0 +1,7 @@
+# Git Time Tracking and Timesheet export
+
+## Setup
+
+* Open your terminal
+* cd to your git repository
+* enter this command `curl -sSL https://raw.githubusercontent.com/yuzu-co/git-timetracking/master/install.sh | bash`
---
+++ install.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+set -e
+
+echo "Installing git-timetracking..."
+pushd `git rev-parse --git-dir`/hooks > /dev/null
+
+if [ -f prepare-commit-msg ] || [ -L prepare-commit-msg ]; then
+    echo "A prepare-commit-msg file already exists."
+    read -p "Overwrite? [y/N] " -n 1 -r < /dev/tty
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        exit 1
+    fi
+    rm -f post-commit
+fi
+
+if [ -d "git-timetracking" ]; then
+    pushd "git-timetracking" > /dev/null
+    git pull origin
+    popd > /dev/null
+else
+    git clone https://github.com/yuzu-co/git-timetracking.git
+fi
+ln -s git-timetracking/prepare-commit-msg prepare-commit-msg
+
+popd > /dev/null
+
+echo "Done!"
