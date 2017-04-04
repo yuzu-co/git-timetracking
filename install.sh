33eadf752f091d503e46b3700f13d9b475cadc97
Olivier Mouren <mouren.olivier@gmail.com>
2017-04-04 11:28:19 +0200

    Fix commit hook

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 echo "Installing git-timetracking..."
 pushd `git rev-parse --git-dir`/hooks > /dev/null
 
-if [ -f prepare-commit-msg ] || [ -L prepare-commit-msg ]; then
+if [ -f commit-msg ] || [ -L commit-msg ]; then
     echo "A prepare-commit-msg file already exists."
     read -p "Overwrite? [y/N] " -n 1 -r < /dev/tty
     echo
@@ -22,7 +22,7 @@ if [ -d "git-timetracking" ]; then
 else
     git clone https://github.com/yuzu-co/git-timetracking.git
 fi
-ln -s git-timetracking/prepare-commit-msg prepare-commit-msg
+ln -s git-timetracking/commit-msg commit-msg
 
 popd > /dev/null
 
